6e5cd5ef889bde69b866716547a31a49e8384b82
Musumba Abbey <Musumba055@gmail.com>
2026-08-08 23:31:56 +0300

    added

--- zalsee-estates/js/google-map.js
+++ zalsee-estates/js/google-map.js
@@ -2,16 +2,12 @@
 var google;
 
 function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
-    // 39.399872
-    // -8.224454
+    // Mbarara City Mall / St. Paul Shopping Mall, Buremba Road.
+    var myLatlng = new google.maps.LatLng(-0.607438, 30.662150);
     
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 7,
+        zoom: 16,
 
         // The latitude and longitude to center the map (always required)
         center: myLatlng,
@@ -34,29 +30,23 @@ function init() {
         ]
     };
 
-    
-
-    // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
+    // Get the HTML DOM element that will contain your map.
     var mapElement = document.getElementById('map');
 
-    // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
-    
-    var addresses = ['New York'];
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: 'Zalseef Estates - St. Paul Shopping Mall'
+    });
 
-    for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
-            var p = data.results[0].geometry.location
-            var latlng = new google.maps.LatLng(p.lat, p.lng);
-            new google.maps.Marker({
-                position: latlng,
-                map: map,
-                icon: 'images/loc.png'
-            });
+    var infoWindow = new google.maps.InfoWindow({
+        content: '<strong>Zalseef Estates</strong><br>St. Paul Shopping Mall, Room C-10<br>Mbarara, Uganda'
+    });
 
-        });
-    }
+    marker.addListener('click', function () {
+        infoWindow.open(map, marker);
+    });
     
 }
 google.maps.event.addDomListener(window, 'load', init);
